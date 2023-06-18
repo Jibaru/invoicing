3d562e03d290001a38dad757d6b7e2df9f697b70
jibaru <ignacioruedaboada@hotmail.com>
2023-06-17 20:31:41 -0500

    feat: update purchase record repository

--- core/PurchaseRecords/Domain/Repositories/PurchaseRecordRepository.php
+++ core/PurchaseRecords/Domain/Repositories/PurchaseRecordRepository.php
@@ -4,6 +4,8 @@ namespace Core\PurchaseRecords\Domain\Repositories;
 
 use Core\PurchaseRecords\Domain\Entities\PurchaseRecord;
 use Core\PurchaseRecords\Domain\Repositories\Specifications\Specification;
+use Core\Vouchers\Domain\Entities\ValueObjects\VoucherID;
+use Exception;
 
 interface PurchaseRecordRepository
 {
@@ -12,4 +14,12 @@ interface PurchaseRecordRepository
     public function getPurchaseRecordsRows(int $page, ?int $perPage = null, ?Specification $specification = null): array;
 
     public function getTotalPages(int $perPage, ?Specification $specification = null): int;
+    public function nextEntryNumber(bool $hasBudget): int;
+
+    /**
+     * @param VoucherID $voucherID
+     * @return PurchaseRecord
+     * @throws Exception
+     */
+    public function getByVoucherID(VoucherID $voucherID): PurchaseRecord;
 }
--- core/PurchaseRecords/Infrastructure/Repositories/MySqlPurchaseRecordRepository.php
+++ core/PurchaseRecords/Infrastructure/Repositories/MySqlPurchaseRecordRepository.php
@@ -2,12 +2,32 @@
 
 namespace Core\PurchaseRecords\Infrastructure\Repositories;
 
+use Carbon\Carbon;
 use Core\PurchaseRecords\Domain\Dtos\PurchaseRecordDTO;
 use Core\PurchaseRecords\Domain\Entities\PurchaseRecord;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\CorrelativeAccountingEntryNumber;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\DailyOperationsTotalAmount;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\DayMonthYearDate;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\Detraction;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\DuaOrDsiIssueYear;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\IgvAmount;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\Period;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\PurchaseRecordID;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\SummaryAmount;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\SupplierDocumentDenomination;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\SupplierDocumentNumber;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\SupplierDocumentType;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\TaxBase;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\UniqueOperationCode;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\VoucherNumber;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\VoucherSeries;
+use Core\PurchaseRecords\Domain\Entities\ValueObjects\VoucherType;
 use Core\PurchaseRecords\Domain\Repositories\PurchaseRecordRepository;
 use Core\PurchaseRecords\Domain\Repositories\Specifications\PeriodSpecification;
 use Core\PurchaseRecords\Domain\Repositories\Specifications\Specification;
 use Core\PurchaseRecords\Infrastructure\Repositories\Facades\MySqlPeriodSpecificationFacade;
+use Core\Vouchers\Domain\Entities\ValueObjects\VoucherID;
+use Exception;
 use Illuminate\Support\Facades\DB;
 
 class MySqlPurchaseRecordRepository implements PurchaseRecordRepository
@@ -62,5 +82,74 @@ class MySqlPurchaseRecordRepository implements PurchaseRecordRepository
 
         $total = $builder->count('id');
         return ceil($total / $perPage);
+    }
+
+    public function nextEntryNumber(bool $hasBudget): int
+    {
+        $currentMaxEntryNumber = DB::table('purchase_records')
+            ->where('has_budget', $hasBudget)
+            ->max('purchase_records.correlative_accounting_entry_number') ?? 0;
+
+        return $currentMaxEntryNumber + 1;
+    }
+
+    /**
+     * @param VoucherID $voucherID
+     * @return PurchaseRecord
+     * @throws Exception
+     */
+    public function getByVoucherID(VoucherID $voucherID): PurchaseRecord
+    {
+        $p = DB::table('purchase_records')
+            ->where('voucher_id', $voucherID->value)
+            ->first();
+
+        if (is_null($p)) {
+            throw new Exception('purchase record not found');
+        }
+
+        return new PurchaseRecord(
+            new PurchaseRecordID($p->id),
+            new VoucherID($p->voucher_id),
+            Period::fromPurchaseRecordFormat($p->period),
+            new UniqueOperationCode($p->unique_operation_code),
+            new CorrelativeAccountingEntryNumber($p->correlative_accounting_entry_number),
+            DayMonthYearDate::fromPurchaseRecordFormat($p->issue_date),
+            isset($p->due_date)
+                ? DayMonthYearDate::fromYearMonthDay($p->due_date)
+                : null,
+            VoucherType::make($p->voucher_type),
+            isset($p->voucher_series)
+                ? new VoucherSeries($p->voucher_series)
+                : null,
+            isset($p->dua_or_dsi_issue_year)
+                ? DuaOrDsiIssueYear::make($p->dua_or_dsi_issue_year)
+                : null,
+            new VoucherNumber($p->voucher_number),
+            isset($p->daily_operations_total_amount)
+                ? DailyOperationsTotalAmount::make($p->daily_operations_total_amount)
+                : null,
+            isset($p->supplier_document_type)
+                ? SupplierDocumentType::make($p->supplier_document_type)
+                : null,
+            isset($p->supplier_document_number)
+                ? SupplierDocumentNumber::make($p->supplier_document_number)
+                : null,
+            isset($p->supplier_document_denomination)
+                ? SupplierDocumentDenomination::make($p->supplier_document_denomination)
+                : null,
+            isset($p->first_tax_base) ? TaxBase::make($p->first_tax_base) : null,
+            isset($p->first_igv_amount) ? IgvAmount::make($p->first_igv_amount): null,
+            isset($p->second_tax_base) ? TaxBase::make($p->second_tax_base) : null,
+            isset($p->second_igv_amount) ? IgvAmount::make($p->second_igv_amount): null,
+            isset($p->third_tax_base) ? TaxBase::make($p->third_tax_base) : null,
+            isset($p->third_igv_amount) ? IgvAmount::make($p->third_igv_amount): null,
+            SummaryAmount::make($p->payable_amount),
+            $p->has_detraction,
+            isset($p->detraction_percentage) ? new Detraction($p->detraction_percentage) : null,
+            $p->has_budget,
+            Carbon::parse($p->created_at),
+            Carbon::parse($p->updated_at),
+        );
     }
 }
