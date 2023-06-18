0ca93af1dbb50a9f857e1742587e4df89740816a
jibaru <ignacioruedaboada@hotmail.com>
2023-06-17 20:30:32 -0500

    feat: update voucher created event

--- core/Vouchers/Application/Events/VoucherCreated.php
+++ core/Vouchers/Application/Events/VoucherCreated.php
@@ -2,14 +2,22 @@
 
 namespace Core\Vouchers\Application\Events;
 
+use Core\BudgetAllocation\Domain\Entities\ValueObjects\CostCenterCode;
 use Core\Vouchers\Domain\Entities\Voucher;
 
 class VoucherCreated
 {
     public readonly Voucher $voucher;
+    public readonly CostCenterCode $costCenterCode;
+    public readonly bool $hasBudget;
 
-    public function __construct(Voucher $voucher)
-    {
+    public function __construct(
+        Voucher $voucher,
+        CostCenterCode $costCenterCode,
+        bool $hasBudget
+    ) {
         $this->voucher = $voucher;
+        $this->costCenterCode = $costCenterCode;
+        $this->hasBudget = $hasBudget;
     }
 }
